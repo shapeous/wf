b5eb4c95655e4914c234fbb7307ecff68ea9d618
Miguel Ulloa <migu71@yahoo.com>
2015-03-18 22:16:43 -0400

    Added Browserify (JQuery and Mustache)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp	= require("gulp")
-	,	gutil	= require("gulp-util")
-	,	coffee= require("gulp-coffee")
-	,	concat= require("gulp-concat");
+var gulp			= require("gulp")
+	,	gutil			= require("gulp-util")
+	,	coffee		= require("gulp-coffee")
+	,	browserify= require("gulp-browserify")
+	,	concat		= require("gulp-concat");
 
 var coffeeSources = ["components/coffee/tagline.coffee"];
 var jsSources			=
@@ -21,5 +22,6 @@ gulp.task("coffee", function() {
 gulp.task("js", function() {
 	gulp.src(jsSources)
 		.pipe(concat("script.js"))
+		.pipe(browserify())
 		.pipe(gulp.dest("builds/development/js"))
 });
