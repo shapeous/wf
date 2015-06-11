7f66578198f94f691994a265fb525ece8a73390c
Miguel Ulloa <migu71@yahoo.com>
2015-06-10 22:36:02 -0400

    Process SASS and Compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp			= require("gulp")
 	,	gutil			= require("gulp-util")
 	,	coffee		= require("gulp-coffee")
 	,	browserify= require("gulp-browserify")
+	,	compass		= require("gulp-compass") 
 	,	concat		= require("gulp-concat");
 
 var coffeeSources = ["components/coffee/tagline.coffee"];
@@ -11,6 +12,7 @@ var jsSources			=
 		,	"components/scripts/tagline.js"
 		,	"components/scripts/template.js"
 		];
+var	sassSources	= ["components/sass/style.scss"];
 
 gulp.task("coffee", function() {
 	gutil.log("- I transpile coffee scripts into JavaScript files.");
@@ -26,4 +28,18 @@ gulp.task("js", function() {
 		.pipe(concat("script.js"))
 		.pipe(browserify())
 		.pipe(gulp.dest("builds/development/js"))
+});
+
+gulp.task("compass", function() {
+	gutil.log("- I transpile SCSS files using gulp-compass");
+	gulp.src(sassSources)
+		.pipe(compass(
+		{	"sass":	"components/sass"
+		,	"image":	"builds/development/images"
+		,	"style":	"expanded"
+		,	"comments":	true
+		})
+			.on("error", gutil.log)
+		)
+		.pipe(gulp.dest("builds/development/css"))
 });
