f6532df57f1d25886fea6558e679e015cfb26eb3
Miguel Ulloa <migu71@yahoo.com>
2015-06-13 21:37:29 -0400

    Setup environment variables to enable ENV specific deployments

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,36 @@ var gulp			= require("gulp")
 	, connect		= require("gulp-connect")
 	,	concat		= require("gulp-concat");
 
-var coffeeSources = ["components/coffee/tagline.coffee"];
-var jsSources			=
+var env
+	,	coffeeSources
+	,	jsSources
+	,	sassSources
+	,	htmlSources
+	,	jsonSources
+	,	outputDir
+	,	sassStyle
+	;
+
+env = process.env.NODE_ENV || "DEV";
+
+if (env === "DEV") {
+	outputDir	= "builds/development/";
+	sassStyle	= "expanded";
+} else {
+	outputDir	= "builds/production/";
+	sassStyle	= "compressed"
+}
+
+coffeeSources = ["components/coffee/tagline.coffee"];
+jsSources			=
 		[	"components/scripts/rclick.js"
 		,	"components/scripts/pixgrid.js"
 		,	"components/scripts/tagline.js"
 		,	"components/scripts/template.js"
 		];
-var	sassSources	= ["components/sass/style.scss"];
-var htmlSources	= ["builds/development/*.html"];
-var jsonSources = ["builds/development/js/*.json"]
+sassSources	= ["components/sass/style.scss"];
+htmlSources	= [outputDir + "*.html"];
+jsonSources = [outputDir + "js/*.json"]
 
 gulp.task("html", function() {
 	gutil.log("- I handle HTML files");
@@ -42,7 +62,7 @@ gulp.task("js", function() {
 	gulp.src(jsSources) // The order of jsSources list controls the processing order.
 		.pipe(concat("script.js"))
 		.pipe(browserify())
-		.pipe(gulp.dest("builds/development/js"))
+		.pipe(gulp.dest(outputDir + "js"))
 		.pipe(connect.reload())
 });
 
@@ -51,13 +71,13 @@ gulp.task("compass", function() {
 	gulp.src(sassSources)
 		.pipe(compass(
 			{	"sass":	"components/sass"
-			,	"image":	"builds/development/images"
-			,	"style":	"expanded"
+			,	"image":	outputDir + "images"
+			,	"style":	sassStyle
 			,	"comments":	true
 			})
 			.on("error", gutil.log)
 		)
-		.pipe(gulp.dest("builds/development/css"))
+		.pipe(gulp.dest(outputDir +"css"))
 		.pipe(connect.reload())
 });
 
@@ -73,7 +93,7 @@ gulp.task("watch", function() {
 gulp.task("connect", function() {
 	gutil.log("- I create a server and reload when something changes");
 	connect.server(
-		{	"root":	"builds/development/"
+		{	"root":	outputDir
 		,	"livereload":	true
 		})
 });
